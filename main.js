917f91fbd808cfe7b3f5b8685ae04f751292a87f
mickael <110117622+mickaelMorais@users.noreply.github.com>
2022-11-04 13:58:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -20,6 +20,6 @@ function novoCarde(data, dia, jogos) {
 }
 
 document.querySelector("#cards").innerHTML=
-    novoCarde('24/11', 'quinta', novoJogo('brasil', '16:00', 'servia'))+
-    novoCarde('28/11', 'segunda', novoJogo('brasil', '13:00', 'suica'))+
-    novoCarde('02/12', 'sexta', novoJogo('camaroes', '16:00', 'brasil'))
+    novoCarde('24/11', 'quinta',novoJogo('suica', '07:00', 'camaroes') + novoJogo('uruguai', '10:00', 'koreiaSul') + novoJogo('portugal', '13:00', 'gana') + novoJogo('brasil', '16:00', 'servia')) +
+    novoCarde('28/11', 'segunda',novoJogo('camaroes', '07:00', 'servia') + novoJogo('koreiaSul', '07:00', 'gana') +  novoJogo('brasil', '13:00', 'suica') + novoJogo('portugal', '16:00', 'uruguai')) + 
+    novoCarde('02/12', 'sexta',novoJogo('koreiaSul', '12:00', 'portugal') + novoJogo('gana', '12:00', 'uruguai') + novoJogo('servia', '07:00', 'suica') + novoJogo('camaroes', '16:00', 'brasil'))
